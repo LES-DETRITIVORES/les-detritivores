fed1863168fb2f285c465af775b5ae854e8ad92a
amineohn <amineprojet7@gmail.com>
2022-01-27 13:24:54 +0100

    fix(tailwind): use content instanted of purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,10 @@ const plugin = require("tailwindcss/plugin");
 module.exports = {
   important: true,
   mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   darkMode: "class",
   theme: {
     extend: {
