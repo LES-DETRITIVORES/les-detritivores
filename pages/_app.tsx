e98eaed25cd0db2a90e377958acd92c70d99ac56
amineohn <amineprojet7@gmail.com>
2022-04-01 10:48:15 +0200

    chore: remove constantsn

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -55,33 +55,6 @@ function MyApp({ Component, pageProps }: AppProps) {
   const { data, error } = useSWR<StoryBlok>("/api/storyblok", fetcher);
   const [play] = useSound(`static/sounds/sound.mp3`);
   const [show, setShow] = useState(false);
-  const products = [
-    {
-      id: 1,
-      name: "Throwback Hip Bag",
-      href: "#",
-      color: "Salmon",
-      price: "$90.00",
-      quantity: 1,
-      imageSrc:
-        "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg",
-      imageAlt:
-        "Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt.",
-    },
-    {
-      id: 2,
-      name: "Medium Stuff Satchel",
-      href: "#",
-      color: "Blue",
-      price: "$32.00",
-      quantity: 1,
-      imageSrc:
-        "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg",
-      imageAlt:
-        "Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.",
-    },
-    // More products...
-  ];
   useEffect(() => {
     if (typeof window !== "undefined") {
       const cookie = window.localStorage.getItem("modalCookie");
