896c747b5079a8870317cbcf2544e10a60edafb7
amineohn <amineprojet7@gmail.com>
2022-04-12 13:09:50 +0200

    pls

--- utils/validator.ts
+++ utils/validator.ts
@@ -1,9 +1,9 @@
 export class Validator {
   constructor() {}
   isEmail(email: string): boolean {
-    const re =
+    const lemme =
       /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    return lemme.test(String(email).toLowerCase());
   }
   isEmpty(value: string): boolean {
     if (value === undefined || value === null || value === "") {
