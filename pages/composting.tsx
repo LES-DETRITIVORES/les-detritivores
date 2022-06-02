c0b276ce4b6680a7459855e40514ef7dc3bd297c
amineohn <amineprojet7@gmail.com>
2022-06-02 14:54:44 +0200

    headlify

--- pages/composting.tsx
+++ pages/composting.tsx
@@ -51,29 +51,29 @@ const Compost: NextPage = () => {
         delay={500}
         distance="30px"
       >
-        <div className="max-w-screen my-3 justify-center content-center">
-          <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-2 sm:grid-cols-1 mx-5 my-2 space-y-2">
+        <div className="content-center justify-center my-3 max-w-screen">
+          <div className="grid grid-cols-1 mx-5 my-2 space-y-2 md:grid-cols-2 xl:grid-cols-2 sm:grid-cols-1">
             <div className="flex flex-col space-y-4">
               {data ? (
                 <>
                   <div className="space-y-4">
-                    <h1 className="text-left pb-2 md:text-6xl text-2xl font-bold text-orangeDTTV -rotate-2">
+                    <h1 className="pb-2 text-2xl font-bold text-left md:text-6xl text-orangeDTTV -rotate-2">
                       COMPOSTAGE
-                      <div className="bg-growing-underline-black hidden">
+                      <div className="hidden bg-growing-underline-black">
                         &nbsp;
                       </div>
                     </h1>
                     <div className="space-y-4">
-                      <p className="text-xl sm:text-md font-bold">
+                      <p className="text-xl font-bold sm:text-md">
                         {richText(data?.content.compostSecondText)}
                       </p>
                       <div className="flex flex-col space-y-2">
-                        <p className="font-light text-xl p-4 space-y-2 pl-10">
+                        <p className="p-4 pl-10 space-y-2 text-xl font-light">
                           {richText(data?.content.compostTreeText)}
                         </p>
                         <div className="inline-flex space-x-2">
                           <Icons icons="check" className="w-5 h-5" />
-                          <p className="font-bold text-xl text-orangeDTTV">
+                          <p className="text-xl font-bold text-orangeDTTV">
                             {richText(data?.content.compostFourText)}
                           </p>
                         </div>
@@ -86,7 +86,7 @@ const Compost: NextPage = () => {
                             "duration-700 ease-in-out group-hover:opacity-75 !rounded-2xl",
                             isLoading
                               ? "scale-110 blur-2xl grayscale !rounded-2xl"
-                              : "scale-100 blur-0 grayscale-0 !rounded-2xl"
+                              : "scale-100 blur-0 grayscale-0 !rounded-2xl",
                           )}
                           width="240"
                           height="180"
@@ -101,7 +101,7 @@ const Compost: NextPage = () => {
                             "duration-700 ease-in-out group-hover:opacity-75 !rounded-2xl",
                             isLoading
                               ? "scale-110 blur-2xl grayscale !rounded-2xl"
-                              : "scale-100 blur-0 grayscale-0 !rounded-2xl"
+                              : "scale-100 blur-0 grayscale-0 !rounded-2xl",
                           )}
                           width="240"
                           height="180"
@@ -116,7 +116,7 @@ const Compost: NextPage = () => {
                             "duration-700 ease-in-out group-hover:opacity-75 !rounded-2xl",
                             isLoading
                               ? "scale-110 blur-2xl grayscale !rounded-2xl"
-                              : "scale-100 blur-0 grayscale-0 !rounded-2xl"
+                              : "scale-100 blur-0 grayscale-0 !rounded-2xl",
                           )}
                           width="240"
                           height="180"
@@ -126,7 +126,7 @@ const Compost: NextPage = () => {
                         />
                       </div>
                     </div>
-                    <span className="flex space-x-2 justify-center 2xl:justify-start xl:justify-start sm:justify-start md:justify-start">
+                    <span className="flex justify-center space-x-2 2xl:justify-start xl:justify-start sm:justify-start md:justify-start">
                       <Icons
                         icons="photo"
                         className="w-6 h-6 text-black fill-current dark:text-white"
@@ -139,14 +139,14 @@ const Compost: NextPage = () => {
                 </>
               ) : (
                 <div className="space-y-4">
-                  <h1 className="text-left pb-4 md:text-6xl text-2xl font-bold text-orangeDTTV animate-pulse -rotate-2 bg-neutral-100 dark:bg-neutral-800 rounded-lg h-20"></h1>
+                  <h1 className="h-20 pb-4 text-2xl font-bold text-left rounded-lg md:text-6xl text-orangeDTTV animate-pulse -rotate-2 bg-neutral-100 dark:bg-neutral-800"></h1>
                   <br />
                   <div className="space-y-6">
-                    <p className="text-xl sm:text-md font-bold bg-neutral-100 dark:bg-neutral-800 rounded-lg h-10 animate-pulse"></p>
+                    <p className="h-10 text-xl font-bold rounded-lg sm:text-md bg-neutral-100 dark:bg-neutral-800 animate-pulse"></p>
                     <div className="flex flex-col space-y-2">
-                      <p className="font-light text-xl p-4 space-y-2 pl-10 rounded-lg animate-pulse bg-neutral-100 h-20"></p>
+                      <p className="h-20 p-4 pl-10 space-y-2 text-xl font-light rounded-lg animate-pulse bg-neutral-100"></p>
                       <div className="inline-flex space-x-2 rounded-lg animate-pulse bg-neutral-100 dark:bg-neutral-800">
-                        <p className="font-bold text-xl text-orangeDTTV h-20"></p>
+                        <p className="h-20 text-xl font-bold text-orangeDTTV"></p>
                       </div>
                     </div>
                   </div>
@@ -161,29 +161,29 @@ const Compost: NextPage = () => {
                       <div className="!rounded-2xl w-60 h-44 animate-pulse bg-neutral-100 dark:bg-neutral-800" />
                     </div>
                   </div>
-                  <span className="flex space-x-2 justify-center 2xl:justify-start xl:justify-start sm:justify-start md:justify-start">
+                  <span className="flex justify-center space-x-2 2xl:justify-start xl:justify-start sm:justify-start md:justify-start">
                     <p className="font-normal text-base mt-0.5 bg-neutral-100 dark:bg-neutral-800"></p>
                   </span>
                 </div>
               )}
             </div>
             <div className="flex flex-col space-y-2">
-              <h1 className="text-center pb-2 md:text-vw text-vw font-bold text-orangeDTTV">
+              <h1 className="pb-2 font-bold text-center md:text-vw text-vw text-orangeDTTV">
                 Vous souhaitez acheter du compost auprès de
                 <br /> la SCIC SA LES DETRITIVORES ?
-                <div className="bg-growing-underline-black hidden">&nbsp;</div>
+                <div className="hidden bg-growing-underline-black">&nbsp;</div>
               </h1>
               <div className="flex flex-col">
-                <button className="bg-greenDTTV transition hover:bg-green-900 dark:hover:bg-orange-600 dark:bg-orangeDTTV text-white p-4 w-60 rounded-full m-auto focus:outline-none">
+                <button className="p-4 m-auto text-white transition rounded-full bg-greenDTTV hover:bg-green-900 dark:hover:bg-orange-600 dark:bg-orangeDTTV w-60 focus:outline-none">
                   <Link href="/quote">
-                    <span className="font-normal text-xl focus:outline-none uppercase">
+                    <span className="text-xl font-normal uppercase focus:outline-none">
                       Commandez-le ici
                     </span>
                   </Link>
                 </button>
                 <span className="text-center text-orangeDTTV">ou</span>
                 <button
-                  className="bg-greenDTTV transition hover:bg-green-900 dark:hover:bg-orange-600 dark:bg-orangeDTTV text-white p-4 w-auto sm:w-auto xl:w-100 2xl:w-100 md:w-auto rounded-full m-auto focus:outline-none"
+                  className="w-auto p-4 m-auto text-white transition rounded-full bg-greenDTTV hover:bg-green-900 dark:hover:bg-orange-600 dark:bg-orangeDTTV sm:w-auto xl:w-100 2xl:w-100 md:w-auto focus:outline-none"
                   onClick={() => {
                     setShowModal(true);
                     setTimeout(() => {
@@ -191,7 +191,7 @@ const Compost: NextPage = () => {
                     }, 5000);
                   }}
                 >
-                  <span className="focus:outline-none font-normal text-md md:text-xl uppercase">
+                  <span className="font-normal uppercase focus:outline-none text-md md:text-xl">
                     Retrouvez-le dans nos magasins partenaires
                   </span>
                 </button>
